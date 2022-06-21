767610cb6191a9f57f1ab8732e6d37a85fbac92f
Anchal <74677360+ANCHAL628@users.noreply.github.com>
2022-06-21 18:05:16 +0530

    Time: 81 ms (7.99%), Space: 27.6 MB (12.75%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int search(vector<int>& arr, int target) {
+        int low = 0 ;
+        int high = arr.size()-1;
+        sort(arr.begin(),arr.end()) ;
+        int ans = -1 ;
+        while(low <= high){
+            int mid = low+(high-low)/2 ;
+            if(arr[mid] == target){
+                ans = mid ;
+                break ;
+            }else if(arr[mid] < target){
+                low = mid+1 ;
+            }else{
+                high = mid-1 ;
+            }
+        }
+       return ans ; 
+    } 
+};
