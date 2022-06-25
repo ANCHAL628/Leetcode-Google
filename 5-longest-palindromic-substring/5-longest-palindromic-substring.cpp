55bbfc44a9b20fc78b0efdd684b241eab85a4ccd
Anchal <74677360+ANCHAL628@users.noreply.github.com>
2022-06-25 13:47:08 +0530

    Time: 28 ms (84.10%), Space: 7.2 MB (82.12%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -5,48 +5,45 @@ public:
       //O(n^2)
       //we will be checking by the length of substring 
        //odd length
-        int maxi = 1 ;
-        int start = 0 ;
-        int end = 0 ;
-      //  int l = 
-        int n = s.size() ;
-        //for odd length
-        for(int i = 0 ; i < n-1 ; i++){
-          int l = i ;
-          int r = i ;
-          while(l >= 0 && r < n){
-              if(s[l] == s[r]){
-                  l-- ;
-                  r++ ;
-             }else{
-                  break ;
-             }
-          }
-         if(maxi < r-l-1){
-            maxi = r-l-1 ;
-            start = l+1 ;
-            end = r-1 ;
-          }
-        }
-        //for even length
+       int start = 0;
+       int end = 0 ;
+       int max_len = 1 ;
+       int n = s.size() ;
+       //for odd length
+       for(int i = 0 ; i < n-1 ; i++){
+           int l =  i ;
+           int r = i ;
+           while(l >= 0 && r < n){
+               if(s[l] == s[r]){
+                   l-- ;
+                   r++ ;
+               }else{
+                   break ;
+               }
+           }
+           if(max_len < r-l-1){
+               start = l+1 ;
+               end = r-1 ;
+               max_len = r-l-1 ;
+           }
+       }
          for(int i = 0 ; i < n-1 ; i++){
-          int l = i ;
-          int r = i+1 ;
-          while(l >= 0 && r < n){
-              if(s[l] == s[r]){
-                  l--;
-                  r++ ;
-             }else{
-                  break ;
-             }
-          }
-         if(maxi < r-l-1){
-            maxi = r-l-1 ;
-            start = l+1 ;
-            end = r-1 ;
-          }
-        }
-        return s.substr(start,maxi) ;
-      
+           int l =  i ;
+           int r = i+1 ;
+           while(l >= 0 && r < n){
+               if(s[l] == s[r]){
+                   l-- ;
+                   r++ ;
+               }else{
+                   break ;
+               }
+           }
+           if(max_len < r-l-1){
+               start = l+1 ;
+               end = r-1 ;
+               max_len = r-l-1 ;
+           }
+       }
+      return s.substr(start,max_len) ;
     }
 };
