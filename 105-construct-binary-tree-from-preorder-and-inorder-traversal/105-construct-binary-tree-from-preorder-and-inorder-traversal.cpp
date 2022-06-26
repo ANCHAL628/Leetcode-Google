6cb59a2c0a112f1c2f60e862e3fd058bf843cf0b
Anchal <74677360+ANCHAL628@users.noreply.github.com>
2022-06-26 10:33:45 +0530

    Time: 23 ms (77.40%), Space: 26.3 MB (46.65%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,6 +11,7 @@
  */
 class Solution {
 public:
+   /* tc: o(N^2)
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
        vector<int>preIndex(1,0) ;
        return constructTree(preorder,inorder,preIndex,0,inorder.size()-1) ;
@@ -33,5 +34,29 @@ public:
         return Node ;
         
         
+    }*/
+  //  tc:O(n)
+   TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+       vector<int>preIndex(1,0) ;
+       unordered_map<int,int>mp ;
+       for(int i = 0 ; i < inorder.size() ; i++){
+           mp[inorder[i]] = i ;
+       }
+       return constructTree(preorder,inorder,preIndex,0,inorder.size()-1,mp) ;
     }
+    TreeNode* constructTree(vector<int>&preorder,vector<int>&inorder,vector<int>&preIndex,int start,int endIndex,unordered_map<int,int>&mp){
+        if(preIndex[0] > preorder.size()-1 || start > endIndex){
+            return NULL ;
+        }
+        int currentval = preorder[preIndex[0]] ;
+        TreeNode* Node = new TreeNode(preorder[preIndex[0]]) ;
+        preIndex[0] += 1 ;
+        int i = mp[currentval] ;
+        Node->left = constructTree(preorder,inorder,preIndex,start,i-1,mp) ;
+        Node->right = constructTree(preorder,inorder,preIndex,i+1,endIndex,mp) ;
+        return Node ;
+        
+        
+    }
+    
 };
