0ec49de4b3fda00be30fe533743c60e82200a046
Anchal <74677360+ANCHAL628@users.noreply.github.com>
2022-07-14 12:11:35 +0530

    Time: 4 ms (97.17%), Space: 15.4 MB (15.25%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.cpp
+++ 946-validate-stack-sequences/946-validate-stack-sequences.cpp
@@ -1,30 +1,15 @@
 class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-  /*   //approach O(n)
-     //space complexity O(n)
-       stack<int>s ;
-       int j = 0 ;
-       for(int i = 0 ; i < pushed.size() ; i++){
-           s.push(pushed[i]) ;
-           while(!s.empty() && s.top() == popped[j]){
-               s.pop() ;
-               j++ ;
-           }
-       }
-        return s.empty() ;*/
-        //approach = O(n)
-        //space complexity = O(1)
-        int k = -1 ;
-        int j = 0  ;
-        for(int i = 0 ; i < pushed.size() ; i++){
-            pushed[++k] = pushed[i] ;
-            while(!(k == -1) && pushed[k] == popped[j]){
-                --k ;
-                j++ ;
-            }
-        }
-        return k == -1 ;
-        
+         stack<int>s1 ;
+         int j = 0 ;
+         for(int i = 0 ; i < pushed.size() ; i++){
+             s1.push(pushed[i]) ;
+             while(!s1.empty() && s1.top() == popped[j]){
+                 s1.pop() ;
+                 j++ ;
+             }
+         }
+        return s1.empty() ;
     }
 };
