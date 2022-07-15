25ab24388a026136231d1fd15ea41b3c3771b5b7
Anchal <74677360+ANCHAL628@users.noreply.github.com>
2022-07-15 15:15:02 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (79.90%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isMatching(char x , char y) {
+        return ((x == '(' && y == ')') || (x == '{' && y == '}')  || (x == '[' && y == ']')) ;
+    }
+    bool isValid(string s) {
+        stack<char>s1 ;
+        for(auto it : s){
+            if(it == '(' || it == '[' || it == '{'){
+                s1.push(it) ;
+            }else{
+                if(s1.empty()){
+                   return false ;
+                }else if(!isMatching(s1.top(),it)){
+                   return false  ;
+                }else{
+                   s1.pop() ;
+                }
+            }
+        }
+        return s1.empty() ;
+    }
+};
