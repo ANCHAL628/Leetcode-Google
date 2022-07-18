fb4082e2043e4db650fff57120b6ce2cc7d846af
Anchal <74677360+ANCHAL628@users.noreply.github.com>
2022-07-18 19:33:09 +0530

    Time: 275 ms (9.52%), Space: 93.4 MB (11.99%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int maximum = 0;
-        int cheapest = prices[0] ;
-        for (int i = 1 ; i<prices.size() ; i++){
-            cheapest = min(cheapest,prices[i]) ;
-            maximum = max(maximum,prices[i]-cheapest) ;
+        //O(n^2) -> i WOUld be checking for all  particular prices and then would be finding one for the next
+        //O(n) ->it simply represent a curve so i would be keeping track of max min value
+        int minVal = prices[0] ;
+        int maxVal = 0 ;
+        for(int i = 1 ; i < prices.size() ; i++){
+           minVal = min(minVal,prices[i]) ;
+           maxVal = max(maxVal,prices[i]-minVal) ;
         }
-        return maximum ;
-       
+        return maxVal ;
     }
 };
