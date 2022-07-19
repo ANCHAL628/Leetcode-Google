71387a7733973cff2321c63d6c1db51e7d897af7
Anchal <74677360+ANCHAL628@users.noreply.github.com>
2022-07-19 20:14:37 +0530

    Time: 10 ms (93.32%), Space: 10.9 MB (46.93%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         //tc : O(n)
-        //1,2
+        //1,2,3
          unordered_map<int,int>mp ;
         vector<int>answer ;
         for(int i = 0 ; i < nums.size() ; i++){
