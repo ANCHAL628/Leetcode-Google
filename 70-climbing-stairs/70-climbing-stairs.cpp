0c9a6bcde8cc70da1e13cfdaccdcf3f2bf8bf9f9
Anchal <74677360+ANCHAL628@users.noreply.github.com>
2022-07-20 13:52:35 +0530

    Time: 6 ms (5.48%), Space: 6.4 MB (15.07%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
+    //O(n)
     int climbStairs(int n) {
+        //recursion as we have choices so whther I will be taking 1 step or two steps and tc : exponential
+      //  unordered_map<int,int>mp ;
+        //tc : O(2^n)
+        //sc : O(n)
+        //Optimal ki tc : O(n)
+        //sc  = O(n);
         unordered_map<int,int>mp ;
-        return climbStair(n,mp) ;
+        return noOfWays(n,0,mp) ;
     }
-    int climbStair(int n,unordered_map<int,int>&mp){
-        if(n == 0){
-            return 1;
+    int noOfWays(int totalStairs,int currentSt,unordered_map<int,int>&mp){
+      //base case having == n
+        if(totalStairs == currentSt){
+            return 1 ;
         }
-        if(n < 0){
+        if(totalStairs < currentSt){
             return 0 ;
         }
-        int currIn = n ;
-        if(mp.find(currIn) != mp.end()){
-            return mp[currIn] ;
+        int currentK= currentSt;
+        if(mp.find(currentSt) != mp.end()){
+            return mp[currentSt] ;
         }
-        mp[currIn] = climbStair(n-1,mp)+climbStair(n-2,mp) ;
-        return mp[currIn] ;
-    }
+        int oneJump = noOfWays(totalStairs,currentSt+1,mp) ;
+        int twoJump = noOfWays(totalStairs,currentSt+2,mp) ;
+        mp[currentSt] = oneJump+twoJump ;
+        return mp[currentSt] ;
+    } 
+    
 };
