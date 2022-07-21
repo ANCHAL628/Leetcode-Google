7847be8b3f64469ae66d2a66027b3510906e13e7
Anchal <74677360+ANCHAL628@users.noreply.github.com>
2022-07-21 12:46:19 +0530

    Time: 1178 ms (16.88%), Space: 244.3 MB (5.01%) - LeetHub

---
+++ 494-target-sum/494-target-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        unordered_map<string,int>mp ;
+        return NoOfWays(nums,target,0,mp) ;
+    }
+    int NoOfWays(vector<int>nums,int target,int currentIn,unordered_map<string,int>&mp){
+        if(currentIn == nums.size() && target== 0){
+            return 1 ;
+        }if(currentIn == nums.size() && target!= 0){
+            return 0 ;
+        }
+           string key = to_string(currentIn)+"-"+to_string(target) ;
+           if(mp.find(key) != mp.end()){
+               return mp[key] ;
+           }
+           int posSign = NoOfWays(nums,target-nums[currentIn],currentIn+1,mp) ;
+           int negSign = NoOfWays(nums,target+nums[currentIn],currentIn+1,mp) ;
+           mp[key] = negSign+posSign ;
+           return mp[key] ;
+    }
+   
+};
