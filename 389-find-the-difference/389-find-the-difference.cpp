c713e2ee906a4484588ff2a1fa66d8c42ce1e945
Anchal <74677360+ANCHAL628@users.noreply.github.com>
2022-08-22 06:04:08 +0530

    Time: 6 ms (43.59%), Space: 6.7 MB (26.37%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-         int Xor = 0 ;
-         for(auto it : s){
-             Xor ^= it ;
-         }
-         for(auto it : t){
-             Xor ^= it ;
-         }
-         return Xor ;
+         //simple iterate through O(n) check if they are equal else return last element
+        //O(n) --> xor
+        //count the frequency which will take O(n) --> timee
+        char Xor = 0 ;
+        for(int i = 0 ; i < s.size() ; i++){
+            Xor ^= s[i] ;
+        }
+        for(int i = 0 ; i < t.size() ; i++){
+            Xor ^= t[i] ;
+        }
+        return Xor ;
     }
 };
