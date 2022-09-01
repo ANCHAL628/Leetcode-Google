0565cf3716f05eaef0c09ddc3cbbb3d770723d7c
Anchal <74677360+ANCHAL628@users.noreply.github.com>
2022-09-01 15:08:31 +0530

    Time: 27 ms (84.33%), Space: 19.3 MB (57.59%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
+    //not worst
     void moveZeroes(vector<int>& nums) {
-       //copy first of all non zeroes eleent then zero and then move it to new vector
-     // tc  O(n),sc O(n)
-    //we will be keeping count of no zeroes eement as soon as we cencounter zero e will be swaping
-   //tc O(n)
-        int count = 0 ; 
+       //O(n)  
+        int count = 0 ;
         for(int i = 0 ; i < nums.size() ; i++){
             if(nums[i] != 0){
-                swap(nums[count],nums[i]) ;
+               swap(nums[i],nums[count]) ;
                 count++ ;
             }
         }
