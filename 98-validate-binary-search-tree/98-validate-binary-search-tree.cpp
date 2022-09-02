133a6fb75529e4aa32e876f167f6a8a666fbeed6
Anchal <74677360+ANCHAL628@users.noreply.github.com>
2022-09-02 10:36:05 +0530

    Time: 20 ms (49.25%), Space: 21.6 MB (70.15%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -11,18 +11,22 @@
  */
 class Solution {
 public:
+    //Worst cast tc : O(n^2)
+    //sc : O(h)
+    /*Best case
+    //extra
     //tc : O(n)
+    //sc : O(h) if function call stack is considered else O(1)*/
     bool isValidBST(TreeNode* root) {
-         bool find = true ;
          return validateBST(root,LONG_MIN,LONG_MAX) ;
     } 
-    bool validateBST(TreeNode* root,long long int left,long long int right){
+    bool validateBST(TreeNode* root,long long int mini,long long int maxi){
          if(root == NULL){
-             return true ;
-         }if((left >= root->val) || (right <= root->val)){
-             return false ;
+           return true ;
          }
-        return validateBST(root->left,left,root->val)&&validateBST(root->right,root->val,right) ;
-         
+        if((mini >= root->val) ||(maxi <= root->val)){
+            return false ;
+        } 
+        return validateBST(root->left,mini,root->val)&&validateBST(root->right,root->val,maxi) ;
     }
 };
