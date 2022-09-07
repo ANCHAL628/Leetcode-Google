37ad91acbf72bd5211bb8e1e440755b8e71b47a9
Anchal <74677360+ANCHAL628@users.noreply.github.com>
2022-09-07 06:31:17 +0530

    Time: 127 ms (13.05%), Space: 30.8 MB (6.75%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-     //sort the string and then add
-        //2nd O(m*m*26)
-        map<vector<int>,vector<string>>mp ;
-        
-        vector<vector<string>>answer ;
-        for(auto it : strs){
-            vector<int>count(26,0) ;
-            //count character in each word
-            for(auto itr : it){
-                count[itr-'a'] += 1 ;
-            }
-            //push them with the cont
-            mp[count].push_back(it) ;
-        }
+       vector<vector<string>>anagrams ;
+       map<vector<int>,vector<string>>mp ;
+       for(auto it : strs){
+          vector<int>count(26,0) ;
+           for(auto itr : it){
+               count[itr-'a'] += 1 ;
+           }
+           mp[count].push_back(it) ;
+       }
         for(auto it : mp){
-            answer.push_back(it.second) ;
+            anagrams.push_back(it.second) ;
         }
-        return answer ;
+        return anagrams ;
     }
 };
