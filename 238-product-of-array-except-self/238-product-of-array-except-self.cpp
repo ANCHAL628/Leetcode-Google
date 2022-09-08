ec7e24d698039da6e80f77c866bec63e10a2e990
Anchal <74677360+ANCHAL628@users.noreply.github.com>
2022-09-08 18:23:48 +0530

    Time: 20 ms (97.71%), Space: 24 MB (68.05%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,22 +1,116 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        /*//use postfix and prefix technique*/
         vector<int>prefix(nums.size(),1) ;
         int product = 1 ;
-      //  nums[0] = 1 ;
-        //prefix multiplication till i-1 so it would be for presentt
         for(int i = 1 ; i < nums.size() ; i++){
             product *= nums[i-1] ;
             prefix[i] = product ;
         }
         product = 1 ;
-        
-        //then go ahead with postfix till i+1 ould be storing for i
-        for(int i = nums.size()-2  ; i >= 0 ; i--){
+        for(int i = nums.size()-2 ; i >= 0 ; i--){
             product *= nums[i+1] ;
             prefix[i] *= product ;
         }
-        return prefix ;
+         
+        return prefix ; 
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
