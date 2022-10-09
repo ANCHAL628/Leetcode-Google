bd2429db2806d7caf339ef54ca43592bcdbbce6f
Anchal <74677360+ANCHAL628@users.noreply.github.com>
2022-10-09 17:26:22 +0530

    Time: 28 ms (74.05%), Space: 18 MB (64.63%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int XOR = 0 ;
+        for(int i = 0 ; i < nums.size() ; i++){
+            XOR ^= nums[i] ;
+        }
+        for(int i = 0 ; i <= nums.size() ; i++){
+            XOR ^= i ;
+        }
+        return XOR ;
+    }
+};
+//
