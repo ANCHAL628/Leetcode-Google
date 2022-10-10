f04da2951b41344c0d27fb5126b6638828b74d5f
Anchal <74677360+ANCHAL628@users.noreply.github.com>
2022-10-10 14:04:04 +0530

    Time: 922 ms (14.72%), Space: 10.1 MB (88.89%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-     //first approach which comes to my mind sis I will be taking each element as single one time complexity O(n^2)
-     //Second a+b  c so we have to find a and b so we can do one thing we can b = c-a we can just find b for each element if present then tc : O(n)
-        unordered_map<int,int>mp ;
-        vector<int>ans ;
-        for(int i = 0 ; i < nums.size() ; i++){
-           if(mp.find(target-nums[i]) != mp.end()){
-              ans.push_back(i)  ;
-              ans.push_back(mp[target-nums[i]]) ;
-              break ;
+       int sum = 0 ;
+       vector<int>fin ;
+       bool found = false ;
+       for(int i = 0 ; i < nums.size() ; i++){
+           for(int j = i+1 ; j < nums.size() ; j++){
+               if(nums[i]+nums[j] == target){
+                   fin.push_back(i) ;
+                   fin.push_back(j) ;
+                   found = true ;
+                   if(found == true){
+                   break ;
+                   }
+               }
            }
-            mp[nums[i]] = i ;
-        }
-        return ans ;
+           if(found == true){
+                   break ;
+            }
+           
+       }
+        return fin ;
     }
 };
