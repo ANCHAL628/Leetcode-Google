ebf7bac2f2b8aefe556dc5472943f0f1d0c72b43
Anchal <74677360+ANCHAL628@users.noreply.github.com>
2022-10-12 17:05:32 +0530

    Time: 262 ms (27.79%), Space: 67.7 MB (90.31%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-      //Naive solution O(N)
+      //optimal solution = O(N)
       int maxi = INT_MIN ;
       int sum = 0 ;
       for(int i = 0 ; i < nums.size() ; i++){
