b9b5f9226b9d20b504e13c530c408a35f7a6a0d5
Anchal <74677360+ANCHAL628@users.noreply.github.com>
2022-10-21 11:38:59 +0530

    Time: 166 ms (54.52%), Space: 19.7 MB (99.80%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,38 +1,42 @@
 class Solution {
 public:
     //O(n^3) -> use three 
+    //O(n^2) -> use two ka square
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>>trip ;
-        if(nums.size() < 3){
-            return trip ;
+      //if size is less than 3 then return it
+        vector<vector<int>>answer ;
+        if(nums.size()<3){
+            return answer ;
         }
         sort(nums.begin(),nums.end()) ;
-        for(int i = 0 ;  i < nums.size() ; i++){
-            if(i == 0 || nums[i-1] != nums[i]){
+        for(int i = 0 ; i < nums.size() ; i++){
+            //removing duplicacy
+            if(i == 0 || (nums[i-1] != nums[i])){
                 int low = i+1 ;
                 int high = nums.size()-1 ;
-                //int findSum = 0-(nums[low]+nums[high]) ;
                 while(low < high){
-                  int sum = nums[i]+nums[low]+nums[high]  ;
-                  if(sum == 0){
-                     trip.push_back({nums[i],nums[low],nums[high]}) ;
-                       while(low < high && nums[low] == nums[low+1]){
-                            low++ ;
-                        }
-                        while(low < high && nums[high] == nums[high-1]){
-                            high-- ;
-                        }
+                    int sum = nums[low]+nums[high]+nums[i] ;
+                    if(sum == 0){
+                       answer.push_back({nums[i],nums[low],nums[high]})  ;
+                    
+                    while(low < high && nums[low+1] == nums[low]){
                         low++ ;
-                        high-- ;
-                  }else if(sum < 0){
-                      low++ ;
-                  }else{
-                      high-- ;
-                  }
+                    }
+                     while(low < high && nums[high-1] == nums[high]){
+                        high--  ;
+                    }
+                    low++ ;
+                    high-- ;
+                    }
+                    else if(sum < 0){
+                        low++ ;
+                    }else{
+                        high--;
+                    }
                 }
             }
         }
-        return trip ;
+        return answer ;
     }
 };
 //
