66221f69a60ce3f283be890391311a3097f7d485
Anchal <74677360+ANCHAL628@users.noreply.github.com>
2022-11-02 22:16:47 +0530

    Time: 8 ms (6.21%), Space: 6.2 MB (95.92%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,25 +2,36 @@ class Solution {
     //tc : O(n)
     //sc : O(n)
 public:
-    bool isMatching(char x , char y) {
-        return ((x == '(' && y == ')') || (x == '{' && y == '}')  || (x == '[' && y == ']')) ;
-    }
-    bool isValid(string s) {
-        stack<char>s1 ;
-        for(auto it : s){
-            if(it == '(' || it == '[' || it == '{'){
-                s1.push(it) ;
-            }else{
-                if(s1.empty()){
-                   return false ;
-                }else if(!isMatching(s1.top(),it)){
-                   return false  ;
-                }else{
-                   s1.pop() ;
-                }
-            }
+   bool isMatching(char topOne, char currntOne){
+        if(topOne == '{' && currntOne == '}'){
+            return true ;
         }
-        return s1.empty() ;
+       if(topOne == '(' && currntOne == ')'){
+            return true ;
+        }
+       if(topOne == '[' && currntOne == ']'){
+            return true ;
+        }
+       return false ;
+   }
+   bool isValid(string s) {
+        stack<char>st ;
+        for(auto i : s){
+           if(i == '{' || i == '(' || i == '['){
+               st.push(i) ;
+           }else{
+               if(st.empty()){
+                   return false;
+               }else if(i == '}' || i == ')' || i == ']'){
+                   if(isMatching(st.top(),i)){
+                       st.pop() ;
+                   }else{
+                       return false ;
+                   }
+                   
+               }
+           }
+        }
+       return (st.empty()) ;
     }
 };
-////
