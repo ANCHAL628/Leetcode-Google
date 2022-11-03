f9ffcc8bf5576793ecaf2eaa9b0d3bfe4a48ade1
Anchal <74677360+ANCHAL628@users.noreply.github.com>
2022-11-03 10:39:46 +0530

    Time: 11 ms (33.77%), Space: 13.7 MB (45.52%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string>parent ;
+        string ans = "" ;
+        generateP(0,0,ans,n,parent) ;
+        return parent ;
+    }
+    void generateP(int currOpen,int closeBrac,string ans,int n,vector<string>&parent){
+        if(currOpen == closeBrac && currOpen == n){
+            parent.push_back(ans) ;
+            return ;
+        }if(currOpen < n){
+            generateP(currOpen+1,closeBrac,ans+'(',n,parent) ;
+        }
+        if(closeBrac < currOpen){
+            generateP(currOpen,closeBrac+1,ans+')',n,parent) ;
+        }
+    }
+};
